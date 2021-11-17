a8b657aa9d13e8081ea0b11cd17c1c2ed049af19
Zach Curtis <zachariahtimothy@gmail.com>
2021-11-17 11:26:14 -0500

    Upgrade and refresh

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 module "eks-jx" {
   source               = "jenkins-x/eks-jx/aws"
-  version              = "1.17.4"
+  version              = "1.18.1"
   cluster_version      = var.cluster_version
   region               = var.region
   vault_user           = var.vault_user
